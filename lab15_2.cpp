0ba53d96a59df85a363b2ab69e9c3a1185dc04ac
AtipatD <atipat_d@cmu.ac.th>
2022-01-11 16:37:17 +0700

    lab15_2.cpp completed

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,37 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double a[][N]){
+	for(int r = 0; r<N; r++){
+		cout << "Row "<< r+1 << ": ";
+		for(int c = 0;c<N; c++){
+			cin >> a[r][c];
+		}
+	}
+}
+
+void findLocalMax(const double a[][N], bool b[][N]){
+	for(int r = 0; r<N; r++){
+		for(int c = 0;c<N; c++){
+			if(r > 0 && r < N-1 && c > 0 && c < N-1)
+				if(a[r][c] < a[r-1][c] || a[r][c] < a[r+1][c] || a[r][c] < a[r][c-1] || a[r][c] < a[r][c+1]){
+					b[r][c] = 0 ;
+				}else{
+					b[r][c] = 1;
+				}
+			else{
+				b[r][c] = 0;
+			}
+		}
+	}
+}
+
+void showMatrix(const bool a[][N]){
+	for(int r = 0; r<N; r++){
+		for(int c = 0; c<N; c++){
+			cout << a[r][c] << " ";
+		}
+		cout << endl;
+	}
+}
